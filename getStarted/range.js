e46229ee867cae56a43bb7f50c6b4552677417a6
luizhf42 <luizhf42@gmail.com>
2023-09-01 15:47:21 -0300

    docs: add the exercise description in the start of the file

--- getStarted/range.js
+++ getStarted/range.js
@@ -1,5 +1,8 @@
+// The range(..) function takes a number as its first argument, representing the first number in a desired range of numbers. The second argument is also a number representing the end of the desired range (inclusive). If the second argument is omitted, then another function should be returned that expects that argument.
+
 const range = (start, end) => {
   if (end === undefined) return (end) => range(start, end);
+
   const result = [];
   for (let i = Number(start) || 0; i <= Number(end); i++) result.push(i);
 
@@ -9,7 +12,7 @@ const range = (start, end) => {
 
 range(3, 3); // [3]
 range(3, 8); // [3,4,5,6,7,8]
-range(3, 0); // [][]
+range(3, 0); // []
 
 const start3 = range(3);
 const start4 = range(4);
